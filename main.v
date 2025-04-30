d99424aab0d0626865ec8516f38e6a399fb5d026
Suguru Inatomi <suguru.inatomi@gmail.com>
2025-04-30 09:16:41 +0900

    feat: handle stdio

--- main.v
+++ main.v
@@ -1,6 +1,33 @@
 module main
 
+import io
+import os
+
 fn main() {
 	msg := 'Hello, V!'
-	println(msg)
+	eprintln(msg)
+
+	// 標準入力からのメッセージを処理する
+	mut stdin_reader := io.new_buffered_reader(reader: os.stdin())
+
+	for {
+		// 入力行を読み取る
+		line := stdin_reader.read_line() or {
+			eprintln('Error reading stdin: ${err}')
+			break
+		}
+
+		// 空行は無視
+		if line.trim_space() == '' {
+			continue
+		}
+
+		// メッセージを処理
+		process_message(line.trim_space())
+	}
+}
+
+fn process_message(message string) {
+	// メッセージの処理を実装
+	eprintln('Processing message: ${message}')
 }
